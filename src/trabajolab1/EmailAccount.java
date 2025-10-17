ec9965c78d1bc2d830723e3c0c0b5402305b4f4e
Axel <axelrene0107@gmail.com>
2025-10-17 10:20:07 -0600

    actualizado-clase-email

--- src/trabajolab1/EmailAccount.java
+++ src/trabajolab1/EmailAccount.java
@@ -4,6 +4,34 @@ package trabajolab1;
  *
  * @author axelr
  */
+
 public class EmailAccount {
+    String direccionEmail;
+    String password;
+    String nombreUsuario;
+    Email[] inbox= new Email[10];
+
+    public EmailAccount(String direccionEmail, String password, String nombreUsuario) {
+        this.direccionEmail = direccionEmail;
+        this.password = password;
+        this.nombreUsuario = nombreUsuario;
+        inbox[]=null;
+    }
+
+    public String getDireccionEmail() {
+        return direccionEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    
+    public static boolean recibirEmail(Email em){
+        
+    }
     
 }
